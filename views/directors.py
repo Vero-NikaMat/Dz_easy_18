c88196049a41004390d46005ace4f77ec9c8ccd8
Ника_комп <v.matuk@skyeng.ru>
2022-01-26 23:27:21 +0300

    Скопировала кое-что похожее

---
+++ views/directors.py
@@ -0,0 +1,24 @@
+# здесь контроллеры/хендлеры/представления для обработки запросов (flask ручки). сюда импортируются сервисы из пакета service
+
+from flask_restx import Resource, Namespace
+
+from models import Director, DirectorSchema
+from setup_db import db
+
+director_ns = Namespace('directors')
+
+
+@director_ns.route('/')
+class DirectorsView(Resource):
+    def get(self):
+        rs = db.session.query(Director).all()
+        res = DirectorSchema(many=True).dump(rs)
+        return res, 200
+
+
+@director_ns.route('/<int:rid>')
+class DirectorView(Resource):
+    def get(self, rid):
+        r = db.session.query(Director).get(rid)
+        sm_d = DirectorSchema().dump(r)
+        return sm_d, 200
